2209b51e7c0888bdd62b95ff105ab709d1d015e8
lujiamin <1207198860@qq.com>
2017-06-08 13:33:55 +0800

    add thread

--- src/example/GetPDF.java
+++ src/example/GetPDF.java
@@ -2,13 +2,18 @@ package example;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.UnknownHostException;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -16,11 +21,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GetPDF {
-	// 使用队列保存 URL
+	// 使用队列和集合保存 URL以及PDFUrl
 	static Queue<String> urlQueue = new LinkedList<>();
-	// 保存PDF的URL集合
+	static HashSet<String> urlSet = new HashSet<>();
 	static HashSet<String> pdfUrlSet = new HashSet<>();
+	static Queue<String> pdfUrlQueue = new LinkedList<>();
+	// 文件命名序号
 	static int i = 1;
+
+	/*
+	 * 使用socket建立连接，并返回网页源码。 但是南邮的网站的传输编码为 chunked 。
+	 * Transfer-Encoding:chunked。无法正确返回网页源码，暂时还不会处理。
+	 * 所以以下代码使用了封装的URLConnection建立连接。 还望老师包容!
+	 */
+
+	public static String socketConn(String host) {
+		String line = "";
+		StringBuffer sb = new StringBuffer();
+		try {
+			InetAddress addr = InetAddress.getByName(host);// 获取IP地址
+			Socket sock = new Socket(addr, 80);
+			BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream(), "utf-8"));
+			wr.write("GET " + '/' + " HTTP/1.1\r\n");
+			wr.write("HOST:" + host + "\r\n");
+			wr.write("Accept:*/*\r\n");
+			wr.write("\r\n\r\n");
+			wr.flush();
+
+			BufferedReader rd = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			while ((line = rd.readLine()) != null) {
+				sb.append(line);
+			}
+			sock.close();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
 
 	// 获取网页的URL
 	public static void getUrl(String target, String rootUrl) {
@@ -45,8 +84,12 @@ public class GetPDF {
 					} else {
 
 					}
-					// 添加进队列
-					urlQueue.offer(urlString);
+					// 如果集合中不存在URL则加入队列
+					if (!urlSet.contains(urlString)) {
+						// 添加进队列
+						urlSet.add(urlString);
+						urlQueue.offer(urlString);
+					}
 				}
 			}
 			bReader.close();
@@ -81,7 +124,11 @@ public class GetPDF {
 			// 找匹配项
 			Matcher matcher = pattern.matcher(stringBuffer);
 			while (matcher.find()) {
-				pdfUrlSet.add(rootUrl + matcher.group());
+				// 如果集合中不存在该url，则加入队列
+				if (!pdfUrlSet.contains(rootUrl + matcher.group())) {
+					pdfUrlSet.add(rootUrl + matcher.group());
+					pdfUrlQueue.offer(rootUrl + matcher.group());
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -121,26 +168,45 @@ public class GetPDF {
 		String rootUrl = "http://pg.njupt.edu.cn";
 		System.out.println("start...");
 		getUrl(rootUrl, rootUrl);
-		// 首页的URL队列
-		int size0 = urlQueue.size();
-		while (size0 > 0) {
-			getUrl(urlQueue.poll(), rootUrl);
-			// System.out.println(size0);
-			size0--;
-		}
-		// 进行一次广度遍历
-		int size1 = urlQueue.size();
-		while (size1 > 0) {
-			getPDFUrl(urlQueue.poll(), rootUrl);
-			size1--;
-		}
-		System.out.println(pdfUrlSet.size());
-		// 遍历PDF的URL集合，进行下载操作
-		for (String string : pdfUrlSet) {
-			getPDF(string);
-		}
-		System.out.println("end!");
+		// 线程1用于爬URL
+		Thread t1 = new Thread() {
 
+			@Override
+			public void run() {
+				while (true) {
+					if (!urlQueue.isEmpty()) {
+						getUrl(urlQueue.peek(), rootUrl);
+						getPDFUrl(urlQueue.peek(), rootUrl);
+						System.out.println(urlQueue.peek());
+						urlQueue.poll();
+					} else {
+						break;
+					}
+				}
+			}
+		};
+		// 线程2用于下载PDF
+		Thread t2 = new Thread() {
+
+			@Override
+			public void run() {
+				while (true) {
+					if (pdfUrlQueue.isEmpty()) {
+						try {
+							sleep(2);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						continue;
+					}
+					System.out.println(pdfUrlQueue.peek());
+					getPDF(pdfUrlQueue.poll());
+				}
+
+			}
+		};
+		t1.start();
+		t2.start();
 	}
 
 	public static void main(String[] args) {
